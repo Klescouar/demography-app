424d40f653611ea50c652994edbb54deedcf64ac
kevin_lescouarnec <klescouarnec@voyages-sncf.com>
2017-10-30 11:56:34 +0100

    First commit

---
+++ src/components/app.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import SearchBar from '../container/search-bar'
+export default class App extends Component {
+  render() {
+    return (
+      <div><SearchBar></SearchBar></div>
+    );
+  }
+}
---
+++ src/container/search-bar.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { getCountries } from '../actions/index'
+
+class SearchBar extends Component {
+    componentWillMount(){
+        this.props.getCountries();
+    }
+
+
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+const mapStateToProps = (state) => {
+    return {
+        countries: state.countries
+    }
+}
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({getCountries}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchBar);
